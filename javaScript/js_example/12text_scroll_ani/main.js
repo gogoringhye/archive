2f650200ea93d4b6e22dc529191efe0f1c5f29ae
gogoringhye <145514996+gogoringhye@users.noreply.github.com>
2023-11-13 16:58:35 +0900

    Add files via upload

---
+++ javaScript/js_example/12text_scroll_ani/main.js
@@ -0,0 +1,13 @@
+let pTag1 = document.querySelector('.first-parallel');
+let pTag2 = document.querySelector('.second-parallel');
+
+let textArrl = 'Welcome Hello Welcome Hello Welcome Hello Welcome Hello'.split(' '); //띄워쓰기를 기준으로 잘라서 배열로 만든다 
+console.log(textArrl)
+
+let textArr2 = "My Portfolio My Portfolio My Portfolio My Portfolio".split(' ');
+console.log(textArr2)
+
+// let arry = [];
+// console.log(...textArrl) //Welcome Hello Welcome Hello Welcome Hello Welcome Hello, 문자가 아니라 요소임
+// arry.push(...textArrl) //...textArrr --> textArrl를 그대로 복사
+// console.log(arr)
